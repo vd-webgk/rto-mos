6484879244a549eff09fef8f9f7ad28501ee9d4c
Admin <git@webgk.ru>
2018-03-26 18:36:25 +0300

    SUP-7584: adding function picture

--- local/templates/aspro_optimus/components/bitrix/catalog.section/catalog_block/result_modifier.php
+++ local/templates/aspro_optimus/components/bitrix/catalog.section/catalog_block/result_modifier.php
@@ -208,6 +208,38 @@ if (!empty($arResult['ITEMS'])){
 					$arOffer['ID'] = intval($arOffer['ID']);
 					if (isset($arDouble[$arOffer['ID']]))
 						continue;
+                        
+                    if ($step == 1){
+                        $OsMin = $arOffer['MIN_PRICE']['VALUE'];
+                    } else {
+                        if ($OsMin > $arOffer['MIN_PRICE']['VALUE']){
+                            $OsMin = $arOffer['MIN_PRICE']['VALUE'];
+                        }
+                    }
+                    $arItem['OsMin'] = $OsMin;
+                    $step++;
+                    //картинка предложения из штрихкода
+                    //debug($arOffer);die();// $arOffer['PROPERTIES']['CML2_BAR_CODE']['VALUE'];
+                    if($arOffer['PROPERTIES']['CML2_BAR_CODE']['VALUE'] > 0)
+                    {
+                        $destinationFile = $_SERVER['DOCUMENT_ROOT'].'/upload/product_images/small/'.$arOffer['PROPERTIES']['CML2_BAR_CODE']['VALUE'].'.jpg';
+                        if(is_file($destinationFile))
+                        {
+                            $arOffer['PREVIEW_PICTURE']['SRC'] = '/upload/product_images/small/'.$arOffer['PROPERTIES']['CML2_BAR_CODE']['VALUE'].'.jpg';
+                        }
+                        elseif (is_file($_SERVER['DOCUMENT_ROOT'].'/upload/product_images/'.$arOffer['PROPERTIES']['CML2_BAR_CODE']['VALUE'].'.jpg'))
+                        {
+                            $sourceFile = $_SERVER['DOCUMENT_ROOT'].'/upload/product_images/'.$arOffer['PROPERTIES']['CML2_BAR_CODE']['VALUE'].'.jpg';
+                            $arSize = array('width'=>240, 'height'=>229);
+                            CFile::ResizeImageFile(
+                                $sourceFile,
+                                $destinationFile,
+                                $arSize,
+                                $resizeType = BX_RESIZE_IMAGE_PROPORTIONAL
+                            );
+                            $arOffer['PREVIEW_PICTURE']['SRC'] = '/upload/product_images/small/'.$arOffer['PROPERTIES']['CML2_BAR_CODE']['VALUE'].'.jpg';
+                        }
+                    }
 					$arRow = array();
 					foreach ($arSKUPropIDs as $propkey => $strOneCode)
 					{
@@ -540,6 +572,28 @@ if (!empty($arResult['ITEMS'])){
 			$arItem = array_merge($arItem, COptimus::formatPriceMatrix($arItem));
 		}
 
+        
+        if($arItem['PROPERTIES']['CML2_BAR_CODE']['VALUE'] > 0)
+            {
+                //debug($arItem['PROPERTIES']['CML2_BAR_CODE']);die();
+                $destinationFile = $_SERVER['DOCUMENT_ROOT'].'/upload/product_images/small/'.$arItem['PROPERTIES']['CML2_BAR_CODE']['VALUE'].'.jpg';
+                if(is_file($destinationFile))
+                {
+                    $arItem['PREVIEW_PICTURE']['SRC'] = '/upload/product_images/small/'.$arItem['PROPERTIES']['CML2_BAR_CODE']['VALUE'].'.jpg';;
+                }
+                elseif (is_file($_SERVER['DOCUMENT_ROOT'].'/upload/product_images/'.$arItem['PROPERTIES']['CML2_BAR_CODE']['VALUE'].'.jpg'))
+                {
+                    $sourceFile = $_SERVER['DOCUMENT_ROOT'].'/upload/product_images/'.$arItem['PROPERTIES']['CML2_BAR_CODE']['VALUE'].'.jpg';
+                    $arSize = array('width'=>240, 'height'=>229);
+                    CFile::ResizeImageFile(
+                        $sourceFile,
+                        $destinationFile,
+                        $arSize,
+                        $resizeType = BX_RESIZE_IMAGE_PROPORTIONAL
+                    );
+                    $arItem['PREVIEW_PICTURE']['SRC'] = '/upload/product_images/small/'.$arItem['PROPERTIES']['CML2_BAR_CODE']['VALUE'].'.jpg';;
+                }
+            }
 		if (
 			$arResult['MODULES']['catalog']
 			&& $arItem['CATALOG']
