168727ce55e04c45f9c3b7678a13e1b8657564d2
vd@webgk.ru <vd@webgk.ru>
2018-09-05 12:19:35 +0300

    sup8364: add snippet, mail template html

--- local/templates/aspro_optimus/snippets/.content.php
+++ local/templates/aspro_optimus/snippets/.content.php
@@ -1,9 +1,9 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
 <?
-if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
-$SNIPPETS = array(
-	'custom/snippet0001.snp' => array('title' => 'Quote with font-size:medium', 'description' => 'Add quote block'),
-	'custom/snippet0002.snp' => array('title' => 'Quote', 'description' => 'Add quote block'),
-	'custom/snippet0003.snp' => array('title' => 'Horizontal line'),
-	'table/snippet0001.snp' => array('title' => 'Table', 'description' => 'Add table with properties'),
-);
+$SNIPPETS = Array();
+$SNIPPETS['custom/snippet0001.snp'] = Array('title' => 'Quote with font-size:medium', 'description' => 'Add quote block');
+$SNIPPETS['custom/snippet0002.snp'] = Array('title' => 'Quote', 'description' => 'Add quote block');
+$SNIPPETS['custom/snippet0003.snp'] = Array('title' => 'Horizontal line');
+$SNIPPETS['table/snippet0001.snp'] = Array('title' => 'Table', 'description' => 'Add table with properties');
+$SNIPPETS['snippet0001.snp'] = Array('title' => 'Новый заказ 2018');
 ?>
